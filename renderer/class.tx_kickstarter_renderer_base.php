e52175b46927ad7efc68298d6f0744af0a22f0e3
gawain <gawain@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-06-10 16:21:12 +0000

    new renderer base class
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kickstartermvc/trunk@5657 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ renderer/class.tx_kickstarter_renderer_base.php
@@ -0,0 +1,56 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c)  2007 Christian Welzel (gawain@camlann.de)  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * @author  Christian Welzel <gawain@camlann.de>
+ */
+
+class tx_kickstarter_renderer_base {
+
+	var $pObj;
+
+	function tx_kickstarter_renderer_base($pObj = 0) {
+		$this->pObj = $pObj;
+	}
+
+   function setParent($pObj) {
+        $this->pObj = $pObj;
+    }
+
+   function generateName($name, $alternative, $prefix, $override) {
+        if(!empty($override))
+            return $override;
+        return ($prefix?$prefix:'').($name?$name:$alternative);
+    }
+
+}
+
+
+// Include ux_class extension?
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/kickstarter__mvc/renderer/class.tx_kickstarter_renderer_base.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/kickstarter__mvc/renderer/class.tx_kickstarter_renderer_base.php']);
+}
+
+?>
