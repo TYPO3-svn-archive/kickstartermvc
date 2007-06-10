aae99d0f36c0991ad9d3599aa12638026ea2864f
gawain <gawain@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-06-10 18:59:52 +0000

    release 0.0.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kickstartermvc/trunk@5658 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "kickstarter__mvc"
 #
-# Auto generated 09-06-2007 16:50
+# Auto generated 10-06-2007 20:58
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A kickstarter plugin that generates code for the lib/div extension development framework.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.0.3',
+	'version' => '0.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"f415";s:10:"README.txt";s:4:"aa0b";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b642";s:21:"template_flexform.xml";s:4:"fa28";s:19:"doc/wizard_form.dat";s:4:"db58";s:20:"doc/wizard_form.html";s:4:"1d08";s:45:"sections/class.tx_kickstarter_section_mvc.php";s:4:"5b10";s:57:"renderer/class.tx_kickstarter_classperaction_renderer.php";s:4:"5ac5";s:58:"renderer/class.tx_kickstarter_methodperaction_renderer.php";s:4:"93fe";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"f415";s:10:"README.txt";s:4:"aa0b";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"bdf4";s:21:"template_flexform.xml";s:4:"fa28";s:19:"doc/wizard_form.dat";s:4:"db58";s:20:"doc/wizard_form.html";s:4:"1d08";s:45:"sections/class.tx_kickstarter_section_mvc.php";s:4:"6dc4";s:52:"sections/class.tx_kickstarter_section_mvc_action.php";s:4:"8906";s:50:"sections/class.tx_kickstarter_section_mvc_base.php";s:4:"9a92";s:51:"sections/class.tx_kickstarter_section_mvc_model.php";s:4:"e2a6";s:54:"sections/class.tx_kickstarter_section_mvc_template.php";s:4:"a9d1";s:50:"sections/class.tx_kickstarter_section_mvc_view.php";s:4:"232a";s:57:"renderer/class.tx_kickstarter_classperaction_renderer.php";s:4:"3742";s:58:"renderer/class.tx_kickstarter_methodperaction_renderer.php";s:4:"18c7";s:47:"renderer/class.tx_kickstarter_renderer_base.php";s:4:"67da";}',
 	'suggests' => array(
 	),
 );
