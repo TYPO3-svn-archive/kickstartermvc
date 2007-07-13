00b8757b62c5a551e1269a2c9493bea06cb84072
gawain <gawain@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-07-13 22:08:55 +0000

    release of 0.0.5 to TER. new ChangeLog file.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/kickstartermvc/trunk@5941 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "kickstarter__mvc"
 #
-# Auto generated 10-06-2007 20:58
+# Auto generated 13-07-2007 23:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Kickstarter for lib/div',
-	'description' => 'A plugin to the kickstarter that generates code for the lib/div extension development framework of ECT.',
+	'description' => 'This is an addon to the kickstarter and generates code for the lib/div extension development framework of ECT. Please report bugs to http://bugs.typo3.org section kickstarter__mvc.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.0.4',
+	'version' => '0.0.5',
 	'dependencies' => 'kickstarter',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,14 +34,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-            'kickstarter' => '0.3.8-',
+			'kickstarter' => '0.3.8-',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"f415";s:10:"README.txt";s:4:"aa0b";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"bdf4";s:21:"template_flexform.xml";s:4:"fa28";s:19:"doc/wizard_form.dat";s:4:"db58";s:20:"doc/wizard_form.html";s:4:"1d08";s:45:"sections/class.tx_kickstarter_section_mvc.php";s:4:"6dc4";s:52:"sections/class.tx_kickstarter_section_mvc_action.php";s:4:"8906";s:50:"sections/class.tx_kickstarter_section_mvc_base.php";s:4:"9a92";s:51:"sections/class.tx_kickstarter_section_mvc_model.php";s:4:"e2a6";s:54:"sections/class.tx_kickstarter_section_mvc_template.php";s:4:"a9d1";s:50:"sections/class.tx_kickstarter_section_mvc_view.php";s:4:"232a";s:57:"renderer/class.tx_kickstarter_classperaction_renderer.php";s:4:"3742";s:58:"renderer/class.tx_kickstarter_methodperaction_renderer.php";s:4:"18c7";s:47:"renderer/class.tx_kickstarter_renderer_base.php";s:4:"67da";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"f415";s:10:"README.txt";s:4:"aa0b";s:12:"ext_icon.gif";s:4:"b4e6";s:17:"ext_localconf.php";s:4:"bdf4";s:21:"template_flexform.xml";s:4:"fa28";s:19:"doc/wizard_form.dat";s:4:"db58";s:20:"doc/wizard_form.html";s:4:"1d08";s:45:"sections/class.tx_kickstarter_section_mvc.php";s:4:"6dc4";s:52:"sections/class.tx_kickstarter_section_mvc_action.php";s:4:"09c7";s:50:"sections/class.tx_kickstarter_section_mvc_base.php";s:4:"15b9";s:51:"sections/class.tx_kickstarter_section_mvc_model.php";s:4:"01c3";s:54:"sections/class.tx_kickstarter_section_mvc_template.php";s:4:"a9d1";s:50:"sections/class.tx_kickstarter_section_mvc_view.php";s:4:"232a";s:57:"renderer/class.tx_kickstarter_classperaction_renderer.php";s:4:"352a";s:58:"renderer/class.tx_kickstarter_methodperaction_renderer.php";s:4:"65b1";s:47:"renderer/class.tx_kickstarter_renderer_base.php";s:4:"4189";}',
 	'suggests' => array(
 	),
 );
